6ee575a34e19f5c0ddc2f9e023336c52e376ff9e
ramachandran-sivanandi <ram710@gmail.com>
2019-06-16 13:38:56 +0530

    cha

--- src/sample1.sh
+++ src/sample1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 echo "hai data is entered"
 echo "hai data is entered ilaiya test33333333:"
+echo "hai data is entered ilaiya test33333334:"
 for testFunction in {1..10}
 do
 echo "Test Message"
